9c9f63e1075b99f7a3c1c72c973ef9e32dadaf05
SherwinKhoo <99573291+SherwinKhoo@users.noreply.github.com>
2022-02-28 19:01:20 +0800

    Added moving lorries.

--- script.js
+++ script.js
@@ -48,28 +48,32 @@ for (let i = x; i < x * (Math.floor(y / 2) - 1) + x; i++) {
 
 let whereIsFrog = -Math.ceil(x / 2); //  index position in the $squares array, from 0 to (x * y)
 
-const movement = (event) => {
+const movementFrog = (event) => {
   $squares.eq(whereIsFrog).removeClass("frogIsHere"); //  remove class from previous position, otherwise, might as well be playing Nokia Snake instead
   switch (event.key) {
     case "ArrowUp":
+      console.log("UP");
       if (whereIsFrog >= x) {
         // cannot move up if on the top row of the grid
         whereIsFrog -= x;
       }
       break; // ALWAYS REMEMBER TO BREAK!!
     case "ArrowDown":
+      console.log("DOWN");
       if (whereIsFrog < x * y - x) {
         // cannot move down if on the bottom row of the grid
         whereIsFrog += x;
       }
       break; // ALWAYS REMEMBER TO BREAK!!
     case "ArrowLeft":
+      console.log("LEFT");
       if (whereIsFrog % x !== 0) {
         // cannot move left if on the leftmost column of the grid
         whereIsFrog -= 1;
       }
       break; // ALWAYS REMEMBER TO BREAK!!
     case "ArrowRight":
+      console.log("RIGHT");
       if ((whereIsFrog + 1) % x !== 0) {
         // cannot move right if on the rightmost column of the grid
         whereIsFrog += 1;
@@ -83,10 +87,107 @@ const movement = (event) => {
 //////////  cars  ////////////////////////////////
 //////////////////////////////////////////////////
 
+for (let i = 0; i < $(".road").length; i++) {
+  if (i < x) {
+    $(".road").eq(i).addClass("lorry"); // top row has lorries moving from right
+  } else if (Math.floor(i / x) % 2 === 1) {
+    $(".road").eq(i).addClass("left"); // odd numbered rows have cars moving from left
+  } else {
+    $(".road").eq(i).addClass("right"); // even numbered rows have cars moving from right
+  }
+}
+
+for (let i = 0; i < $(".river").length; i++) {
+  if (i < x) {
+    $(".river").eq(i).addClass("lorry"); // top row has lorries moving from right
+  } else if (Math.floor(i / x) % 2 === 1) {
+    $(".river").eq(i).addClass("left"); // odd numbered rows have cars moving from left
+  } else {
+    $(".river").eq(i).addClass("right"); // even numbered rows have cars moving from right
+  }
+}
+
+for (let i = 0; i < $(".lorry").length; i++) {
+  $(".lorry")
+    .eq(i)
+    .addClass(`${i % x}`);
+}
+for (let i = 0; i < $(".left").length; i++) {
+  $(".left")
+    .eq(i)
+    .addClass(`${i % x}`);
+}
+for (let i = 0; i < $(".right").length; i++) {
+  $(".right")
+    .eq(i)
+    .addClass(`${i % x}`);
+}
+
+let whereIsLorry1Front = 2 * x - 7;
+let whereIsLorry1Back = 2 * x - 6;
+
+let whereIsLorry2Front = 2 * x - 2;
+let whereIsLorry2Back = 2 * x - 1;
+
+const movementNPC = () => {
+  $(".lorry").eq(whereIsLorry1Front).removeClass("frontIsHere");
+  $(".lorry").eq(whereIsLorry1Back).removeClass("backIsHere");
+
+  $(".lorry").eq(whereIsLorry2Front).removeClass("frontIsHere");
+  $(".lorry").eq(whereIsLorry2Back).removeClass("backIsHere");
+
+  // front of lorry
+  if (whereIsLorry1Front > 0) {
+    whereIsLorry1Front -= 1;
+  } else {
+    whereIsLorry1Front += 2 * x;
+  }
+  $(".lorry").eq(whereIsLorry1Front).addClass("frontIsHere");
+
+  // back of lorry
+  if (whereIsLorry1Back > 0) {
+    whereIsLorry1Back -= 1;
+  } else {
+    whereIsLorry1Back += 2 * x;
+  }
+  $(".lorry").eq(whereIsLorry1Back).addClass("backIsHere");
+
+  // front of lorry
+  if (whereIsLorry2Front > 0) {
+    whereIsLorry2Front -= 1;
+  } else {
+    whereIsLorry2Front += 2 * x;
+  }
+  $(".lorry").eq(whereIsLorry2Front).addClass("frontIsHere");
+
+  // back of lorry
+  if (whereIsLorry2Back > 0) {
+    whereIsLorry2Back -= 1;
+  } else {
+    whereIsLorry2Back += 2 * x;
+  }
+  $(".lorry").eq(whereIsLorry2Back).addClass("backIsHere");
+
+  // cars from the left
+
+  // cars from the right
+};
+
+//////////////////////////////////////////////////
+//////////  timer  ///////////////////////////////
+//////////////////////////////////////////////////
+
+//////////////////////////////////////////////////
+//////////  river  ///////////////////////////////
+//////////////////////////////////////////////////
+
 //////////////////////////////////////////////////
 //////////  window onload  ///////////////////////
 //////////////////////////////////////////////////
 
 $(() => {
-  $("body").keyup(movement);
+  $("body").keyup(movementFrog);
+  setInterval(() => {
+    movementNPC();
+  }, 1000);
 });
